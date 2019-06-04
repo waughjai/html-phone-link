2a567e3ef7cde8f9488ff661775dc0041058bf29
Jaimeson Waugh <jaimeson@4cesi.com>
2019-06-04 13:51:05 -0700

    Update TestHashItem dependency

--- src/HTMLPhoneLink.php
+++ src/HTMLPhoneLink.php
@@ -1,24 +1,22 @@
 <?php
 
 declare( strict_types = 1 );
-namespace WaughJ\HTMLPhoneLink
+namespace WaughJ\HTMLPhoneLink;
+
+use WaughJ\HTMLLink\HTMLLink;
+
+class HTMLPhoneLink extends HTMLLink
 {
-	use WaughJ\HTMLLink\HTMLLink;
-	use function WaughJ\TestHashItem\TestHashItemExists;
-
-	class HTMLPhoneLink extends HTMLLink
+	public function __construct( $phone_number, array $other_attributes = [] )
 	{
-		public function __construct( $phone_number, array $other_attributes = [] )
-		{
-			$phone_number = ( string )( $phone_number );
-			$href = "tel:" . $this->formatPhoneNumberForURL( $phone_number );
-			$value = TestHashItemExists( $other_attributes, 'value', $phone_number );
-			parent::__construct( $href, $value, $other_attributes );
-		}
+		$phone_number = ( string )( $phone_number );
+		$href = "tel:" . $this->formatPhoneNumberForURL( $phone_number );
+		$value = $other_attributes[ 'value' ] ?? $phone_number;
+		parent::__construct( $href, $value, $other_attributes );
+	}
 
-		private function formatPhoneNumberForURL( string $phone_number ) : string
-		{
-			return preg_replace( '/([^0-9]*)/', '', $phone_number );
-		}
+	private function formatPhoneNumberForURL( string $phone_number ) : string
+	{
+		return preg_replace( '/([^0-9]*)/', '', $phone_number );
 	}
 }
--- tests/HTMLPhoneLinkTest.php
+++ tests/HTMLPhoneLinkTest.php
@@ -20,8 +20,8 @@ class HTMLPhoneLinkTest extends TestCase
 	public function testMailLinkWithCustomValueAndOtherLinkAttributes()
 	{
 		$link = new HTMLPhoneLink( '( 454 ) - 212 - 8102', [ "value" => "Give us a ring", "class" => "phone-link" ] );
-		$this->assertContains( ' href="tel:4542128102', $link->getHTML() );
-		$this->assertContains( ' class="phone-link', $link->getHTML() );
+		$this->assertStringContainsString( ' href="tel:4542128102', $link->getHTML() );
+		$this->assertStringContainsString( ' class="phone-link', $link->getHTML() );
 		$this->assertThat( $link->getHTML(), $this->logicalNot( $this->stringContains( ' value="Give us a ring"' ) ) );
 	}
 }
